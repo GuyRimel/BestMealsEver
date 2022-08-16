942cc4ba89f6632e807ae4b4ce1a0a5f46259a99
GuyRimel <53277832+GuyRimel@users.noreply.github.com>
2022-08-16 09:25:19 -0500

    added a loop that evaluates pokemon heights

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 let pokemonList = [
   {
     name: 'Bulbasaur',
-    height: 2,
+    height: 1.49,
     types: [
       'grass',
       'poison'
@@ -9,7 +9,7 @@ let pokemonList = [
   },
   {
     name: 'Charmander',
-    height: 3,
+    height: 5,
     types: [
       'fire',
       'orange lizard'
@@ -27,6 +27,22 @@ let pokemonList = [
   }
 ];
 
-let poke0 = pokemonList[0];
+// This loop evaluates the height of each pokemon in the pokemonList array and writes a string to the document.
+// the 'poke' value is set to pokemon object at the current index
+// the 'phrase' value is determined by the pokemon's height
+let poke;
+let phrase;
+for (i = 0; i < pokemonList.length; i++) {
+  poke = pokemonList[i];
+  if (poke.height > 3) {
+    phrase = 'Woa, that\'s big!';
+  } else if (poke.height < 1.5) {
+    phrase = 'Pft, that\'s tiny.'
+  } else {
+    phrase = 'That\'s about average size.'
+  }
 
-document.write(`${poke0.name} is ${poke0.height} feet tall and is a ${poke0.types[0]} type.`);
+  document.write(
+    `${poke.name} is ${poke.types[0]}y and ${poke.height} feet tall. ${phrase}<br>`
+  );
+}
