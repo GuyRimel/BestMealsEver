b5ff1c85d30a2cba6c379093d8d5d973a5f04a93
GuyRimel <135guyrimel@gmail.com>
2022-08-17 15:10:43 -0500

    Task 1.5 Part 1 Submission

--- js/scripts.js
+++ js/scripts.js
@@ -33,111 +33,20 @@ let pokemonList = [
 // This loop evaluates the height of each pokemon in the pokemonList array and writes a string to the document.
 // the 'poke' value is set to pokemon object at the current index
 // the 'phrase' value is determined by the pokemon's height
-let poke;
-let phrase;
-for (i = 0; i < pokemonList.length; i++) {
-  poke = pokemonList[i];
-  if (poke.height > 3) {
+pokemonList.forEach(
+  function(poke) {
+    let phrase;
+    
+    if (poke.height > 3) {
     phrase = 'Woa, that\'s big!';
-  } else if (poke.height < 1.5) {
-    phrase = 'Pft, that\'s tiny.'
-  } else {
-    phrase = 'That\'s about average size.'
-  }
+    } else if (poke.height < 1.5) {
+      phrase = 'Pft, that\'s tiny.'
+    } else {
+      phrase = 'That\'s about average size.'
+    }
 
-  document.write(
-    `${poke.name} is ${poke.types[0]}y and ${poke.height} feet tall. ${phrase}<br>`
-  );
-}
-
-function add(v1, v2) {
-  return v1 + v2;
-}
-let number = add(3, 5);
-console.log(add(4, 2) + number);
-
-let message = function() {
-  console.log(`The number is ${number}`);
-}
-
-message();
-
-let sayHello = function(firstName) {
-  console.log('Hello ' + firstName)
-}
-
-sayHello("Bobby");
-sayHello();
-
-function divide(dividend, divisor) {
-  if (divisor == 0) {
-    return 'you are trying to divide by zero, DUMMY.';
-  } else {
-    let result = dividend / divisor;
-    return result;
-  }
-}
-
-console.log(divide(4, 2));
-console.log(divide(7, 0));
-console.log(divide(12, -2));
-console.log(divide(12, 5));
-
-// a function with a return statement
-function add(num1, num2) {
-  return num1 + num2;
-}
-
-let result = add(5, 7);
-console.log(result);
-
-pokemonList[2].speak();
-
-let multiplyBy5 = num => num * 5;
-
-console.log(multiplyBy5(3));
-
-let multiplyTwoNumsAnd10 = (num1, num2) => {
-  return num1 * num2 * 10;
-}
-
-console.log(multiplyTwoNumsAnd10(3, 5));
-
-console.warn('you are in DANGER!');
-console.error('something went WRONG.');
-
-function pokeSpeak() {
-  pokemonList[2].speak();
-}
-
-// setTimeout(pokeSpeak, 5000);
-
-let pets = [];
-let pet1 = {
-  name: 'Pokey'
-}
-
-pets.push(pet1.name);
-pets.push('Miles', 'Miles', 'Milesh');
-pets.push('Sandy');
-pets.push('Ash', 'Dumpy', 'Stumpy', 'Dumpty');
-
-console.log(pets);
-console.log(pets.indexOf('Milesh'));
-
-pets.forEach(
-  function(name) {
-    console.log(name)
-  }
+    document.write(
+      `${poke.name} is ${poke.types[0]}y and ${poke.height} feet tall. ${phrase}<br>`
+    );
+    }
 );
-
-
-(function() {
-  
-  myArray = [1,2,3,4,5,6,7,8,9,10,11,12];
-  let str ='';
-
-  myArray.forEach( item => str += `<div>I am div ${item}</div>` );
-
-  document.getElementById('container').innerHTML = str;
-})()
