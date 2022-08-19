0d0e18234d41f7742b9235bc1fb1ad6d1b86c8b2
GuyRimel <53277832+GuyRimel@users.noreply.github.com>
2022-08-18 19:17:53 -0500

    added appending functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,37 @@
-// mealRepository function (IIFE)
-let mealRepository = (
+// recipeRepository function (IIFE)
+let recipeRepository = (
   function() {
-    let mealList = [
+    // the first object ( recipeList[0] ) is used as validation for the 'addv()' method
+    let recipeList = [
       {
-        price: '$$$',
-        name: 'Steamed Snowcrab Legs',
+        name: 'Snowcrab Legs',
+        description: "Snowcrab legs are the most delicious thing ever. They are super easy to steam or bake!",
         categories: ['seafood', 'healthy', 'meat'],
-        rating: 10
+        ingredients: ['crab', 'butter', 'garlic powder'],
+        price: '$$$', // could be calculated on the fly with ingredient prices and a formula...
+        recipe: [],
+        rating: 10,
+        reviews: [{},{}]
       }
     ];
 
-    // 'add' pushes one argument to the mealList array WITHOUT validation
-    function add(meal) {
-      mealList.push(meal);
-      console.log(`${meal.name} meal added!`);
+    // 'add' pushes one argument to the recipeList array WITHOUT validation
+    function add(recipe) {
+      recipeList.push(recipe);
+      console.log(`${recipe.name} recipe added!`);
     }
 
-    // 'addv' pushes input to the mealList array WITH validation
-    // the first object in mealList is considered 'valid'
+    // 'addv' pushes input to the recipeList array WITH validation
+    // the first object in recipeList is considered 'valid'
     // the 'every' method is used to compare the sorted 'input' argument with sorted 'validInput' (comparing sorted 'Object.keys()' arrays) and returns a boolean
     function addv(input) {
-      let validInput = Object.keys(mealList[0]).sort();
+      let validInput = Object.keys(recipeList[0]).sort();
 
       if(typeof(input) == 'object') {
         let inputArray = Object.keys(input).sort();
 
         if(inputArray.every((value, index) => value === validInput[index])) {
-          mealRepository.add(input);
+          recipeRepository.add(input);
         } else {
           console.log(`input=${input.name} is an object with incorrect keys...`)
         }
@@ -36,37 +41,41 @@ let mealRepository = (
       }
     }
 
-    // returns the mealList array
+    // returns the recipeList array
     function getAll() {
-      return mealList;
+      return recipeList;
     }
 
-    function writeMeal(meal) {
-      document.write(`${meal.name}<br>`);
-      document.write(`Price: ${meal.price}<br>`);
-      document.write(`Rating: ${phraseRating(meal.rating)}<br>`);
+    function appendrecipe(recipe) {
+      let parentElement = document.querySelector('.container');
+      let element = document.createElement('div');
+      let elementString = `${recipe.name}\nCategories: ${recipe.categories}\nPrice: ${recipe.price}\nRating: ${recipe.rating} ${phraseRating(recipe.rating)}\n`;
+
+      element.innerText = elementString;
+      element.classList.add('recipeTile');
+      parentElement.appendChild(element);
     }
 
     function phraseRating(rating) {
       let phrase;
       
       if (rating >= 8) {
-        phrase = `"${rating}" This is awesome!`;
+        phrase = `This is awesome!`;
       } else if (rating < 8 && rating >= 5) {
-        phrase = `"${rating}" this is pretty good!`;
+        phrase = `This is pretty good!`;
       } else if (rating < 5 && rating >= 3){
-        phrase = `"${rating}" this is not very good...`;
+        phrase = `This is not very good...`;
       } else {
-        phrase = `"${rating}" GROSS!`;
+        phrase = `GROSS!`;
       }
 
       return phrase;
     }
 
-    // this function takes a query string and returns a filtered array from mealList names
+    // this function takes a query string and returns a filtered array from recipeList names
     function filterByName(query){
       let nameArray = [];
-      mealList.forEach(el => nameArray.push(el.name));
+      recipeList.forEach(el => nameArray.push(el.name));
       return nameArray.filter((el) => el.toLowerCase().includes(query.toLowerCase()));
     }
 
@@ -75,7 +84,7 @@ let mealRepository = (
       add: add,
       addv: addv,
       getAll: getAll,
-      writeMeal: writeMeal,
+      appendrecipe: appendrecipe,
       phraseRating: phraseRating,
       filterByName: filterByName
     }
@@ -86,20 +95,37 @@ let mealRepository = (
 // the 'poke' value is set to pokemon object at the current index
 // the 'phrase' value is determined by the pokemon's height
 
-mealRepository.addv('rat-stew');
+recipeRepository.addv('rat-stew');
 
-mealRepository.addv(
+recipeRepository.addv(
   {
     name: 'mouse-stew',
-    cccategories: ['gross', 'typo'],
+    categories: ['gross', 'made with dead mice'],
     price: '$',
-    rating: 2
+    rating: 2,
+    reviews: [],
+    description: '',
+    ingredients: [],
+    recipe: []
   }
-  );
+);
 
-mealRepository.addv({name: "cereal", rating: 7.9, price: '$', categories:['cheap']});
+recipeRepository.addv(
+  {
+    name: 'apple-pie',
+    categories: ['gross', 'made with dead mice'],
+    price: '$',
+    rating: 2,
+    reviews: [],
+    description: '',
+    ingredients: [],
+    recipe: []
+  }
+);
 
-mealRepository.addv(
+recipeRepository.addv({name: "cereal", rating: 7.9, price: '$', categories:['cheap']});
+
+recipeRepository.addv(
   {
     name: 'dog-stew',
     categories: [],
@@ -108,7 +134,7 @@ mealRepository.addv(
   }
 );
 
-mealRepository.addv(
+recipeRepository.addv(
   {
     name: 'poisonous mushrooms flambee',
     categories: [],
@@ -117,7 +143,7 @@ mealRepository.addv(
   }
 );
 
-mealRepository.addv(
+recipeRepository.addv(
   {
     name: 'blue mushrooms',
     categories: ['blue'],
@@ -126,10 +152,10 @@ mealRepository.addv(
   }
 );
 
-mealRepository.getAll().forEach(
-  function(meal) {
-    mealRepository.writeMeal(meal);
+recipeRepository.getAll().forEach(
+  function(recipe) {
+    recipeRepository.appendrecipe(recipe);
     }
 );
 
-document.write(`<br>Search Results: ${mealRepository.filterByName('mushroom')}`);
+document.write(`<br>Search Results: ${recipeRepository.filterByName('e')}`);
