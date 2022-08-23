e2f068a54dd8885e5e0ef45c180c6f86d2e37bb7
GuyRimel <53277832+GuyRimel@users.noreply.github.com>
2022-08-23 11:35:12 -0500

    added loading message

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,17 @@
 // pokemonRepository function (IIFE)
-let pokemonRepository = (function() {
+let pokemonRepository = (function () {
   // the first object ( pokemonList[0] ) is used as validation for the 'addv()' method
   let pokemonList = [];
-  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+  let limit = 150;
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=' + limit;
 
   function loadList() {
-    return fetch(apiUrl).then(function(response) {
+    showLoadingMessage();
+
+    return fetch(apiUrl).then(function (response) {
       return response.json();
-    }).then(function(json) {
+      
+    }).then(function (json) {
       json.results.forEach(function (item) {
         let pokemon = {
           name: item.name,
@@ -15,68 +19,68 @@ let pokemonRepository = (function() {
         };
         add(pokemon);
       });
+
+      hideLoadingMessage();
+
     }).catch(function(e) {
       console.error(e);
-    })
+      hideLoadingMessage();
+    });
   }
 
+  // fetches details from an object with a "detailsUrl"
   function loadDetails(item) {
     let url = item.detailsUrl;
-    return fetch(url).then(function(response) {
+
+    showLoadingMessage();
+
+    return fetch(url).then(function (response) {
       return response.json();
-    }).then(function(details) {
+
+    }).then(function (details) {
       // now add details to the item
       item.imageUrl = details.sprites.front_default;
       item.height = details.height;
       item.types = details.types;
+    
+      hideLoadingMessage();
     }).catch(function (e) {
       console.error(e);
+      hideLoadingMessage();
     });
   }
   
   function showDetails(pokemon) {
-    loadDetails(pokemon).then(function() {
-      console.log(pokemon)}).catch(function() {
-        console.log(`${pokemon} failed to show...`);
+    loadDetails(pokemon).then(function () {
+      console.log(pokemon)}).catch(function () {
+        console.log(`failed to show ${pokemon}`);
       });
     }
     
-      function addListItem(pokemon) {
-        let listItem = document.createElement('li');
-        let button = document.createElement('button');
-    
-        button.innerText = `${pokemon.name}`;
-        button.classList.add('pokemonTile');
-        button.addEventListener('click', function(event) {
-          showDetails(pokemon);
-          });
-    
-        listItem.appendChild(button);
-        document.querySelector('.pokemon-list').appendChild(listItem);
-      }
-    
-  // 'add' pushes one argument to the pokemonList array WITHOUT validation
-  function add(pokemon) {
-    pokemonList.push(pokemon);
+  function addListItem(pokemon) {
+    let listItem = document.createElement('li');
+    let button = document.createElement('button');
+
+    button.innerText = `${pokemon.name}`;
+    button.classList.add('pokemonTile');
+    button.addEventListener('click', function (event) {
+      showDetails(pokemon);
+      });
+
+    listItem.appendChild(button);
+    document.querySelector('.pokemon-list').appendChild(listItem);
   }
-
-  // 'addv' pushes input to the pokemonList array WITH validation
-  // the first object in pokemonList is considered 'valid'
-  // the 'every' method is used to compare the sorted 'input' argument with sorted 'validInput' (comparing sorted 'Object.keys()' arrays) and returns a boolean
-  function addv(input) {
-    let validInput = Object.keys(pokemonList[0]).sort();
-
-    if(typeof(input) == 'object') {
-      let inputArray = Object.keys(input).sort();
-
-      if(inputArray.every((value, index) => value === validInput[index])) {
-        pokemonRepository.add(input);
-      } else {
-        console.log(`input=${input.name} is an object with incorrect keys...`)
-      }
-
-    } else {
-      console.log(`input=${input} was not valid.`)
+    
+  // pushes a pokemon object with validated key names to the pokemonList array
+  function add(pokemon) {
+    if (
+      typeof(pokemon) === 'object' &&
+      'name' in pokemon &&
+      'detailsUrl' in pokemon
+    ) {
+      pokemonList.push(pokemon);
+    }else{
+      console.log(`failed to add ${pokemon}`);
     }
   }
 
@@ -84,7 +88,21 @@ let pokemonRepository = (function() {
   function getAll() {
     return pokemonList;
   }
+
+  function showLoadingMessage() {
+    let element = document.createElement('div');
+
+    element.classList.add('loading-message');
+    element.innerText = 'Rounding up those pesky Pokemon...';
+    document.querySelector('ul.pokemon-list').appendChild(element);
+  }
+
+  function hideLoadingMessage() {
+    let element = document.querySelector('div.loading-message');
+    document.querySelector('ul.pokemon-list').removeChild(element);
+  }
   
+  // evaluates the height and returns a string
   function phraseHeight(height) {
     let phrase;
     
@@ -104,19 +122,19 @@ let pokemonRepository = (function() {
   // ! -- only the things in this returned object are accessible outside the scope of this function
   return {
     add: add,
-    addv: addv,
     getAll: getAll,
     addListItem: addListItem,
     phraseHeight: phraseHeight,
     showDetails: showDetails,
     loadList: loadList,
-    loadDetails: loadDetails
+    loadDetails: loadDetails,
+    showLoadingMessage: showLoadingMessage
   }
 })();
 
 pokemonRepository.loadList().then(function() {
   //data loaded in from API
-  pokemonRepository.getAll().forEach(function(pokemon) {
-    pokemonRepository.addListItem(pokemon);
-  });
+    pokemonRepository.getAll().forEach(function(pokemon) {
+      pokemonRepository.addListItem(pokemon);
+    });
 });
